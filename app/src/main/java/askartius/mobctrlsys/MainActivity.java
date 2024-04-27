7874f6ba3512f6222500bc3e6eea790ba0ef92eb
Askartius <151680098+askartius@users.noreply.github.com>
2024-04-27 17:43:07 +0300

    Create overall environment: - Add bottom navigation - Add home tab

--- app/src/main/java/askartius/mobctrlsys/MainActivity.java
+++ app/src/main/java/askartius/mobctrlsys/MainActivity.java
@@ -1,8 +1,14 @@
 package askartius.mobctrlsys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.color.DynamicColors;
+import com.google.android.material.navigation.NavigationBarView;
 
 import askartius.mobctrlsys.databinding.ActivityMainBinding;
 
@@ -13,7 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        DynamicColors.applyToActivityIfAvailable(this);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.navigationBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navigation_home) {
+                    binding.pager.setCurrentItem(0);
+                } else if (item.getItemId() == R.id.navigation_graphs) {
+                    binding.pager.setCurrentItem(1);
+                } else if (item.getItemId() == R.id.navigation_process) {
+                    binding.pager.setCurrentItem(2);
+                } else if (item.getItemId() == R.id.navigation_motion) {
+                    binding.pager.setCurrentItem(3);
+                }
+                return false;
+            }
+        });
+
+        binding.pager.setAdapter(new PagerAdapter(this));
+        binding.pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                binding.navigationBar.getMenu().getItem(position).setChecked(true);
+            }
+        });
     }
 }
---
+++ app/src/main/java/askartius/mobctrlsys/PagerAdapter.java
@@ -0,0 +1,38 @@
+package askartius.mobctrlsys;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import askartius.mobctrlsys.ui.HomeFragment;
+
+public class PagerAdapter extends FragmentStateAdapter {
+    private final List<Fragment> fragments = new ArrayList<>();
+    public PagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+        fragments.add(new HomeFragment());
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position < fragments.size()) {
+            return fragments.get(position);
+        } else {
+            return fragments.get(0);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
