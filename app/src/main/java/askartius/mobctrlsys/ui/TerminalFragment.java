62cdf7975771e40eb6a6ad1b0f2f0f23599c684d
Askartius <151680098+askartius@users.noreply.github.com>
2024-05-21 09:56:06 +0300

    Remove unnecessary import

--- app/src/main/java/askartius/mobctrlsys/ui/TerminalFragment.java
+++ app/src/main/java/askartius/mobctrlsys/ui/TerminalFragment.java
@@ -15,7 +15,6 @@ import android.view.ViewGroup;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 
-import askartius.mobctrlsys.MainActivity;
 import askartius.mobctrlsys.R;
 import askartius.mobctrlsys.api.EspComms;
 import askartius.mobctrlsys.api.EspCommsViewModel;
